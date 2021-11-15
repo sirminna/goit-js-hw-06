79a2ddf1940c71be86346a5c5d4658a02752d1ef
sirminna <innasrmn@gmail.com>
2021-11-15 21:52:15 +0200

    08
    1

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,15 @@
-const validInput = document.querySelector('input');
-const inputLen = validInput.getAttribute('data-length')
-const inputId = validInput.getAttribute('id')
-console.log(validInput)
-console.log(inputLen)
-console.log(inputId)
-// console.log(validInput.data-length)
-validInput.addEventListener('blur', onInputBlur);
+const validInput = document.getElementById("validation-input");
+const inputLen = validInput.getAttribute('data-length')    
+console.log(validInput.value.length);
+      validInput.addEventListener('blur', onInputBlur);
 
-      function onInputBlur(event) {
-         
-if (validInput.value.length  === inputLen) {
- console.log('hfhfhf');
-//validInput.setAttribute('id', 'validation-input.valid');
-}
-    console.log('Инпут потерял фокус - событие blur');
-  }
-  
+      function onInputBlur(event)  { 
+            console.log(validInput.value.length);        
+      if (inputLen > validInput.value.length) { 
+       validInput.classList.remove('valid');
+        validInput.classList.add('invalid');
+      } else {
+        validInput.classList.remove('invalid');
+        validInput.classList.add('valid');
+      }
+    };
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+console.log(email)
+console.log(password)
+form.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+if (email === '')
+alert('UHGY') 
+  const formData = new FormData(event.currentTarget);
+
+  console.log(formData);
+
+  formData.forEach((email, password) => {
+    console.log("email");
+   
+ });
+}
